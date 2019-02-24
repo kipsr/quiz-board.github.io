4ae5afa6b6e7e103e26fa3a98e76b2e33769b26a
kipkorir cornelius <mishrakiprop@gmail.com>
2019-02-24 20:13:30 +0300

    add variable

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,8 @@ $("document").ready(function(){
 function Quiz(){
     var correct=0;
     for(var i = 1; i<=5;i++){
-      var radios= document.getElementsByID('id');
-      for (var z=0; z<radios.length; z++){
+      var radios= document.getElementsByName('name');
+      for (var z=00; z<radios.length; z++){
         var radio =radios[z];
         if(radio.value=== "correct" && radio.checked){
   
