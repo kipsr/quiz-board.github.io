d4d281a9aa64141f0829b2a09e60c20d07fab64f
kipkorir cornelius <mishrakiprop@gmail.com>
2019-02-24 20:04:54 +0300

    add varibles

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,16 @@ $("document").ready(function(){
 });
 
 
-
-
+function Quiz(){
+    var correct=0;
+    for(var i = 1; i<=5;i++){
+      var radios= document.getElementsByID('id');
+      for (var z=0; z<radios.length; z++){
+        var radio =radios[z];
+        if(radio.value=== "correct" && radio.checked){
+  
+        }
+      }
+    }
+        alert("correct Responses: "+answersCorrect);
+  }
