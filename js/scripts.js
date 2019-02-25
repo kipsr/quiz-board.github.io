ebf67438bc2fb3da995e284b37c43af9f0bec546
kipkorir cornelius <mishrakiprop@gmail.com>
2019-02-25 09:52:40 +0300

    add js styes

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@ $("document").ready(function(){
     
 });
 $(document).ready(function() {
-  $("#quiz").submit(function(event){
+  $("#test").submit(function(event){
     event.preventDefault();
-    var q1 = parseInt($("[name=answer1]:checked").val());
-    var q2 = parseInt($("[name=answer2]:checked").val());
-    var q3 = parseInt($("[name=answer3]:checked").val());
-    var q4 = parseInt($("[name=answer4]:checked").val());
-    var q5 = parseInt($("[name=answer5]:checked").val());
+    var question1 = parseInt($("[name=Q1]:checked").val());
+    var question2 = parseInt($("[name=Q2]:checked").val());    
+    var question3= parseInt($("[name=Q3]:checked").val());
+    var question4= parseInt($("[name=Q4]:checked").val());
+    var question5= parseInt($("[name=Q5]:checked").val());
     
  
-    var score = parseInt(q1+q2+q3+q4+q5+q6+q7+q8+q9+q10+q11+q12);
-    if (score>20) {
+    var score = parseInt(Q1+Q2+Q3+Q4+Q5);
+    if (score>25) {
       $("#input").text("Your Result is: " + score + " Superb");
     } else {
       $("#input").text("Your score is: " + score + " pull up your socks!");
