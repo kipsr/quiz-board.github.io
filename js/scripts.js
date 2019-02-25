74daadc728944f7fa39d35fb4a5726b8db6054a5
kipkorir cornelius <mishrakiprop@gmail.com>
2019-02-25 07:16:51 +0300

    remove class

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,3 @@ $("document").ready(function(){
 });
 
 
-function Quiz(){
-    var answercorrect=0;
-    
-    for(var i = 1; i<=5;i++){
-      var radios= document.getElementsByName('name');
-      for (var z=0; z<radios.length; z++){
-        var radio =radios[z];
-        if(radio.value=== "correct" && radio.checked){
-  
-        }
-      }
-    }
-        alert("correct Responses: "+answersCorrect);
-  }
